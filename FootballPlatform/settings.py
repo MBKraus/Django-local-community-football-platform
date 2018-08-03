74f8784ecc5a4f4d18ec47f649467b5f4569d033
Mike Kraus <mkraus@live.nl>
2018-08-03 09:28:39 +0200

    Update settings.py

--- FootballPlatform/settings.py
+++ FootballPlatform/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'so9$*5gn&b(db8x&8u0-6&t_+2zys_=z!x$qe95rq!vj$okv*v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
